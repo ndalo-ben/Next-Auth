e1086b026100c044b53840aec114d4f5dd9acf87
ben <ndaloben8@gmail.com>
2024-03-02 08:53:06 +0300

    auth options complete

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { AuthOptions } from 'next-auth';
 import prisma from '@/lib/prisma';
+import * as bcrypt from 'bcrypt';
+import NextAuth from 'next-auth/next';
 
 
 
@@ -22,16 +24,29 @@ export const authOptions: AuthOptions = {
                 }
             },
 
-            async authorize(credentials){
+            async authorize(credentials) {
                 const user = await prisma.user.findUnique({
                     where: {
                         email: credentials?.username
                     }
                 });
 
-                if(!user) throw new Error("Username or password is incorrect");
-                
+                if (!user) throw new Error("Username or password is incorrect");
+
+                if (!credentials?.password) throw new Error("Please Provide a password");
+                const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
+                if (!isPasswordCorrect) throw new Error("Username or password is incorrect");
+
+                const { password, ...userWithoutPass } = user;
+                return userWithoutPass;
+
             }
         })
     ]
 }
+
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
