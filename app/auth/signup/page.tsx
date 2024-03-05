5887cb2d3ad3339f6ae48d25bc0e3292945e7bf8
ben <ndaloben8@gmail.com>
2024-03-05 22:58:00 +0300

    signup form

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,14 +1,22 @@
-import { Link } from "@nextui-org/react"
+import SignUpForm from "@/app/components/SignUpForm"
+import { Image, Link } from "@nextui-org/react"
 
 const SignupPage = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
-            <div className="md:cols-span-2 flex justify-center items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center items-center gap-3">
+            <div className="md:col-span-2 flex justify-center items-center">
                 <p className="text-center p-2">Already Signed up?</p>
                 <Link href={"/auth/signin"}>
                     Sign In
                 </Link>
             </div>
+            <SignUpForm />
+            <Image
+                src="/register.svg"
+                alt="signup"
+                width={500}
+                height={500}
+            />
         </div>
     )
 }
--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -1,9 +1,52 @@
-import { Input } from "@nextui-org/react"
+"use client";
+
+import { EnvelopeIcon, EyeIcon, EyeSlashIcon, KeyIcon, PhoneIcon, UserIcon } from "@heroicons/react/20/solid"
+import { Button, Checkbox, Input } from "@nextui-org/react"
+import { useState } from "react";
 
 const SignUpForm = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // show/hide password 
+  const toggleVisible = () => setIsVisible(prev => !prev);
+
   return (
-    <form>
+    <form className="grid grid-cols-2 gap-3 p-2 shadow place-self-stretch border rounded-md">
+      <Input label="First Name" startContent={<UserIcon className="w-4" />} />
+      <Input label="Last Name" startContent={<UserIcon className="w-4" />} />
       <Input
+        label="Email"
+        className="col-span-2"
+        startContent={<EnvelopeIcon className="w-4" />}
+      />
+      <Input
+        label="Phone"
+        className="col-span-2"
+        startContent={<PhoneIcon className="w-4" />}
+      />{""}
+      <Input
+        type={isVisible ? "text" : "password"}
+        label="Password"
+        className="col-span-2"
+        startContent={<KeyIcon className="w-4" />}
+        endContent={isVisible ? (
+          <EyeIcon onClick={toggleVisible} className="w-4 cursor-pointer" />
+        ) : (
+          <EyeSlashIcon onClick={toggleVisible} className="w-4 cursor-pointer" />
+        )}
+      />
+      <Input
+        type={isVisible ? "text" : "password"}
+        label="Confirm Password"
+        className="col-span-2"
+        startContent={<KeyIcon className="w-4" />}
+      />
+      <Checkbox className="col-span-2">
+        I Accept the <a href="#" className="underline">Terms</a>
+      </Checkbox>
+      <div className="flex justify-center col-span-2">
+        <Button className="w-48" color="primary">Submit</Button>
+      </div>
     </form>
   )
 }
