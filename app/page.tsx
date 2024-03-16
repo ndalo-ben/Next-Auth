4a814c1db4aa9440f038baf17cd6b834438af97e
ben <ndaloben8@gmail.com>
2024-03-16 22:23:32 +0300

    verification email setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { sendMail } from "@/lib/mail";
 
-export default function Home() {
+export default async function Home() {
+
+  await sendMail({
+    to: "techwolf54@gmail.com",
+    subject: "Test",
+    body: "Test"
+  });
   return (
     <div>
       <p>Home Page</p>
